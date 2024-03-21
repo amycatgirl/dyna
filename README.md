d46a00bace5dbed74d2a298bd9be847cf4cb7243
Amy <amy@amogus.cloud>
2024-03-21 19:01:16 -0400

    chore: release source code to the public

--- README.md
+++ README.md
@@ -1,28 +1,123 @@
-# Revite Plugin Boilerplate
+# dyna (beta)
+updater for revite plugins, manager coming as soon as the API is stabilized (aka. in a few months)
 
-> ## Beware that plugins *will* break whenever the [client rewrite](https://github.com/revoltchat/frontend) is generaly available.
-> Alongside the client rewrite, plugins will probably have a different format, as opposed to json. A discussion related to the
-> topic is available [here.](https://github.com/revoltchat/frontend/issues/252)
+## why dyna
 
-Boilerplate for manifest V1 revite plugins. Originally [revite-ts-plugin-boilerplate](https://github.com/sussycatgirl/revite-ts-plugin-boilerplate),
-adapted for Vanilla JS.
+i was annoyed at the fact that i had to send my plugin each time i made a change
 
-## Instructions
+it was also highly requested too
 
-A nix shell is available with this repo, to use it, run `nix-shell`, the shell comes with all the things necesary for development.
+## installing it
 
-If you don't want to use `nix`, you need to have `node` and `pnpm ` installed. Instalation through `corepack` is highly recommended.
+open the console
 
-## Building
+type `state.plugins.add()`, then copy the contents of this repo's `plugin.json` and paste them inside the parenthesis
 
-To build, use the following command.
+dyna does not have a ui yet
 
+it also triggers every hour or so
 
-```bash
+## auto updating with dyna
 
-pnpm build
-
-# Or if you want an unminified build
-# pnpm build:dev
-  
+- add a `dyna.json` file with the following contents:
+```jsonc
+{
+  "latest": 1, // version that dyna will compare to
+  "target": "target/plugin.json" // (optional) location of the plugin inside the repo
+}
 ```
+- add a dyna object to your plugin's manifest with the following:
+```jsonc
+{
+  // ...
+  "dyna": {
+    "repo": "username/gh-repo",
+    "shouldRestart": false // reload the entire client after update
+  }
+}
+```
+
+that's it
+quite literally
+
+info: this might change in the future
+
+## spec
+
+### dyna manifest
+
+`version`: plugin's current version (INT)
+
+`latest`: latest available version from git forge (INT)
+
+`dev`: plugin is on dev mode (BOOLEAN, OPTIONAL)
+
+`repo`: plugin's repository (STRING)
+
+`shouldRestart`: restart client after update (BOOLEAN)
+
+### dyna api
+
+`version`: plugin's current version (INT)
+
+`latest`: latest available version from git forge (INT)
+
+`dev`: plugin is on dev mode (BOOLEAN)
+
+`repo`: plugin's repository (STRING)
+
+`shouldRestart`: restart client after update (BOOLEAN)
+
+`author`: plugin's author (STRING)
+
+`id`: plugin id (STRING)
+
+## caveats
+
+- doesn't support other git providers/forges
+- no manager
+- two files to allow updates
+- no dependency management
+- no manual updates
+- etc, etc, etc
+- just check the road map to see what is missing
+- feature requests welcome :)
+
+## roadmap
+
+- [x] proof-of-concept updater
+- [x] release cod
+- [ ] updater v1
+  - [x] allow other plugins to see dyna-compatible plugins
+  - [ ] merge dyna.json with manifest.json
+  - [ ] update indicator
+  - [x] manual updates for other plugins
+  - [ ] update check without downloading plugin
+- [ ] manager v1
+  - [ ] allow downloading other plugins in-app
+  - [ ] make central repo with dyna manifests
+  - [ ] manual update toggle per plugin (by user)
+  - [ ] manual update button
+- [ ] updater v2
+  - [ ] allow other git providers such as gitea and forgejo
+  - [ ] dependencies
+    - [ ] download
+  - [ ] event emitters for other plugins to listen to
+  - [ ] deprecation notice warning
+  - [ ] self-update
+  - [ ] plugin types
+- [ ] manager v2
+  - [ ] paste url to download
+  - [ ] search per author
+  - [ ] dependency listing
+  - [ ] display plugin types
+
+## contributing
+
+uhhhh
+
+just don't do malicious code and be friendly to others inside issues/pull requests/etc
+
+## licence
+
+project licenced under the mit licence as usual
--- src/index.js
+++ src/index.js
@@ -1,6 +1,231 @@
 () => {
-  // This is your plugin's entrypoint, code you type here will be ran as soon as the client starts up
-  // regardless of whether the user's session started.
+	/**
+	 * Dyna plugin information, should contain latest version to compare against
+	 * @typedef {Object} DynaPluginInformation
+	 * @property {string} version - Latest version of the plugin
+	 * @property {string} [target="plugin.json"] - Optional target to download the plugin
+	 */
 
-  console.log("Heyo! I am a sample plugin!");
+	/**
+	 * Information about a currently loaded plugin
+	 * @typedef {Object} DynaPlugin
+	 * @property {string} author- Namespace of the plugin, usually the author's username.
+	 * @property {string} id- Id of the plugin.
+	 * @property {number} repo - Location of the plugin's repository.
+	 * @property {int} version - Currently loaded version.
+	 * @property {boolean} [dev=false] - Whether the plugin is currently running in developer mode.
+	 * @property {boolean} [shouldRestart=false] - Whether the manager should reload the client when the plugin is updated.
+	 */
+
+	/** Dyna */
+	window.dyna = {};
+
+	/**
+	 * Dyna's store. It stores currently loaded plugins and the interval on which the plugins should be updated
+	 * Default interval is 1h
+	 * @type {{ dynaplugins: DynaPlugin[], updateInterval: number }}
+	 */
+	window.dyna.store = { dynaplugins: [], updateInterval: 3600 }
+
+	/** Convert the interval from seconds to milliseconds */
+	const CALCULATED_INTERVAL = window.dyna.store.updateInterval * 1000;
+
+	/** Used for the loading indicator, which is not implemented yet */
+	let updateProgress = 0;
+
+
+	/**
+	 * Internal API - Fetch a remote resourse whilst logging it's progress.
+	 * Percentage streaming is planned
+	 */
+	async function fetchWithProgress(url) {
+		const xhr = new XMLHttpRequest();
+		return await new Promise((resolve) => {
+			xhr.addEventListener("progress", (event) => {
+				if (event.lengthComputable) {
+					console.log("Downloading:", `${(event.loaded / event.total) * 100}%`);
+				}
+			});
+			xhr.addEventListener("loadend", () => {
+				if (xhr.readyState === 4 && xhr.status === 200) {
+					resolve(xhr.responseText);
+				}
+			});
+			xhr.open("GET", url);
+			xhr.send();
+		});
+	}
+
+	/**
+	 * Internal API - Update dyna's plugin store
+	 */
+	function gatherPluginsFromState() {
+		// Reset store because it's going to be recalculated
+		if (window.dyna.store.dynaplugins.length > 0) {
+			window.dyna.store.dynaplugins.length = 0
+		}
+
+		const data = state.plugins.plugins.data_; // map of K=name+instance, V=plugin
+		for (const [key, value] of data.entries()) {
+			if (value.value_.dyna) {
+				if (value.value_.namespace === "amycatgirl" && (value.value_.id === "dyna-devel" || value.value_.id === "dyna") && value.value_.dyna.dev) {
+					allowDebugging()
+				}
+
+				window.dyna.store.dynaplugins.push({
+					...value.value_.dyna,
+					version: value.value_.version,
+					author: value.value_.namespace,
+					id: value.value_.id
+				});
+			}
+		}
+
+		console.log("Plugins using dyna:", window.dyna.store.dynaplugins.length)
+	}
+
+	/**
+	 * Not implemented - Show a modal prompting the user to restart their client
+	 */
+	function restartWithModalWarning() {
+		console.log("TODO")
+	}
+
+	/**
+	 * Internal API - Fetch plugin information from Github
+	 * @param {string} repo - Plugin's repository
+	 * @returns {DnyaPluginInformation}
+	 */
+	async function fetchPluginInformation(repo) {
+		try {
+			const res = fetch(`https://raw.githubusercontent.com/${repo}/main/dyna.json`).then(async (res) => await res.json())
+
+			return res
+		} catch (err) {
+			console.error("Error getting plugin information for repo:", repo)
+		}
+	}
+
+	/**
+	 * Internal API - Parse and load plugins, also handle syntax errors/plugin errors
+	 * @param {string} pl - Plugin manifest, in the form of a stringified json object
+	 */
+	function parseAndLoadPlugin(pl) {
+		try {
+			const plugin = JSON.parse(pl)
+			console.log("PARSED", plugin)
+			window.state.plugins.add(plugin)
+		} catch(err) {
+			console.error("Could not load plugin:", err)
+		}
+	}
+
+
+	/**
+	 * Internal API - Update all installed plugins.
+	 */
+	async function updateAllPlugins() {
+		// Check if the store isn't empty or was initialized incorrectly
+		if (window.dyna.store.dynaplugins.length == 0) {
+			gatherPluginsFromState()
+		}
+
+		let shouldRestart = false;
+		for await (const [index, plugin] of window.dyna.store.dynaplugins.entries()) {
+			const info = await fetchPluginInformation(plugin.repo)
+			if (info.latest > plugin.version) {
+				if (plugin.dev) {
+					console.log("plugin in dev mode, not updating")
+					continue;
+				}
+
+				if (plugin.shouldRestart) {
+					shouldRestart = true
+				};
+
+				try {
+					const updatedPlugin = await fetchWithProgress(`https://raw.githubusercontent.com/${plugin.repo}/main/${info.target ?? "plugin.json"}`)
+					parseAndLoadPlugin(updatedPlugin)
+				} catch(err) {
+					console.error(err)
+				}
+			} else {
+				console.log("Plugin is up-to-date :)")
+			}
+
+			updateProgress = ((index + 1) / window.dyna.store.dynaplugins.length) * 100
+			console.log(`Progress: ${updateProgress}%`)
+		}
+
+		if (shouldRestart) {
+			// TODO: Replace with restartWithModalWarning
+			window.alert("RELOADING")
+			window.location.reload();
+		}
+	}
+
+	/**
+	 * Internal API - Expose internal APIs to debug dyna
+	 */
+	function allowDebugging() {
+		// Only for debugging purposes
+		window.dyna.debug = {};
+		window.dyna.debug.updatePluginsForced = updateAllPlugins;
+		window.dyna.debug.updateStore = gatherPluginsFromState;
+		window.dyna.debug.fetch = fetchWithProgress;
+		window.dyna.debug.parse = parseAndLoadPlugin;
+		window.dyna.debug.getPluginInformation = fetchPluginInformation;
+	}
+
+	/**
+	 * Dyna plugin API
+	 */
+	window.dyna.api = {}
+
+	/**
+	 * Returns an array of plugins that are using Dyna
+	 * @returns {DynaPlugin[]}
+	 */
+	window.dyna.api.getPlugins = function () {
+		const pluginArray = window.dyna.store.dynaplugins;
+		return Object.freeze(pluginArray)
+	}
+
+	/**
+	 * Check for plugin updates
+	 * @param {string} author - Author of the plugin to search
+	 * @param {string} id - Id of the plugin to search
+	 * @returns {boolean} whether the plugin needs an update or the check has failed
+	 */
+	window.dyna.api.checkUpdatesForPlugin = async function (author, id) {
+		try {
+			const plugin = window.dyna.store.dynaplugins.find(pl => pl.author == author && pl.id == id);
+
+			/** @type {DynaPluginInformation} */
+			const information = await fetchPluginInformation(plugin.repo)
+
+			if (plugin.version != information.latest && !plugin.dev) {
+				console.log(`[updater] An update has been found for the plugin ${author}/${id}`);
+				return true
+			}
+
+			console.log(`[updater] No update has been found...`)
+			return false
+		} catch (error) {
+			console.error("[updater] An error has been thrown:", error)
+			return false
+		}
+	}
+
+	setInterval(updateAllPlugins, CALCULATED_INTERVAL)
+	setTimeout(gatherPluginsFromState, 500) // Let plugins load successfully
+
+	return  {
+		onUnload: () => {
+			// Unload dyna by removing the update interval and removing all related APIs from the window object
+			clearInterval(updateAllPlugins, CALCULATED_INTERVAL)
+			window.dyna = null
+			console.log("DYNA has been unloaded...")
+		}
+	}
 }
